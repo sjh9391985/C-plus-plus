d9e32ab6284f0c7884f7ff52188cfe22904b9585
Son JaeHyeon <sjh9391985@gmail.com>
2022-06-08 22:59:38 +0900

    c++ auto type

---
+++ C++/sty04.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+
+	// auto : 자동으로 데이터 타입을 결정
+	// auto 형을 자주 사용하는 습관은 지양
+
+	auto n = 100;	// int type
+	auto f = 1.5;	// float type
+	auto ll = 1.3e12L;	// long long type
+
+	return 0;
+}
