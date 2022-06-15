73be05a5b29e74fd9ac7e1d5cf3375f299eec804
Son JaeHyeon <sjh9391985@gmail.com>
2022-06-15 21:23:52 +0900

    재귀함수

---
+++ C++/sty15.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void countDown(int);
+
+int main() {
+
+	// C++에서 함수는 자기 자신을 호출할 수 있습니다. : 재귀호출, 재귀함수
+	countDown(5);
+
+}
+
+void countDown(int n) {
+	cout << "Counting..." << n << endl;
+	if (n > 0) 
+		countDown(n - 1);
+	cout << n << "번째 재귀함수" << endl;
+}
